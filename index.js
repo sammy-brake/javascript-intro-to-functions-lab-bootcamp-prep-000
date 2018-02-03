62e9af7e92707ea744003018cb27ecae7badbc92
Samantha brake <sammy.brake@gmail.com>
2018-02-03 20:53:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
-    return "I can't hear you!";
-} else if (string === string.toUpperCase()) 
+    return "I can't hear you!" ;
+} else if (string === string.toUpperCase()) {
+    return "YES INDEED" ;
+} else (string.toLowerCase() === string) {
+    return "I love you, too."
+}
   
